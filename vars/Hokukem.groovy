e7e92aa51e7e91a16f1d10e43027dd50a3135ed6
KemkemGobir <106024545+KemkemGobir@users.noreply.github.com>
2022-06-30 02:48:07 +0100

    Create Hokukem.groovy

---
+++ vars/Hokukem.groovy
@@ -0,0 +1,14 @@
+def call (string mavengoal){
+  if ("${mavengoal}"=="Build")
+  {
+    sh "mvn clean package"
+  }
+  else if ("${mavengoal}" == "Test")
+  {
+  sh "mvn sonar:sonar"
+  }
+  else if ("${mavengoal}" == "BackupNexus")
+  {
+  sh "mvn deploy"
+  }
+}
