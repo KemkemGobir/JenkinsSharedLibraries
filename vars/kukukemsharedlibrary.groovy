7e8cc12b665ffa7067af166dc3cdfc3a99e0e475
KemkemGobir <106024545+KemkemGobir@users.noreply.github.com>
2022-07-02 19:14:38 +0100

    Update and rename kukukemsharedlibrary to kukukemsharedlibrary.groovy

---
+++ vars/kukukemsharedlibrary.groovy
@@ -0,0 +1,18 @@
+def call (string nodejsgoal){
+  if ("$ {nodejsgoal}" == "Build") 
+  {
+    sh "echo Building Application"
+    sh "npm install"
+  }
+  else if ("${nodejsgoal}" == "Test"
+           {
+              sh "echo Code testing"
+             sh "npm run sonar"
+           }
+           else if ("${nodejsgoal}" == "Backup")
+           {
+             sh "echo Backup"
+             sh "npm publish"
+           }
+  }
+           
