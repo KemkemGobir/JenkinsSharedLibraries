2f5c3aba887ce62c5ca8d3b5bad9ea9b63a65313
KemkemGobir <106024545+KemkemGobir@users.noreply.github.com>
2022-07-02 20:16:01 +0100

    Update kukukemsharedlibrary.groovy

--- vars/kukukemsharedlibrary.groovy
+++ vars/kukukemsharedlibrary.groovy
@@ -1,17 +1,14 @@
 def call (string nodejsgoal){
   if ("${nodejsgoal}" == "Build") 
   {
-    sh "echo Building Application"
     sh "npm install"
   }
   else if ("${nodejsgoal}" == "Test"
            {
-              sh "echo Code testing"
              sh "npm run sonar"
            }
            else if ("${nodejsgoal}" == "Backup")
            {
-             sh "echo Backup"
              sh "npm publish"
            }
   }
